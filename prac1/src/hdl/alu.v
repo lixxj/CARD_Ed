78c57ce6b4d0dc3ad35d28ff57e6f3c78297beb1
XJ Li <49367624+lixxj@users.noreply.github.com>
2019-10-01 15:46:49 +0100

    Update alu.v

--- prac1/src/hdl/alu.v
+++ prac1/src/hdl/alu.v
@@ -9,8 +9,6 @@
 // Description:    Integer ALU for RISC-V core (RV32I version)
 //
 // This module is expected to implement an RV32I ALU.
-// This file initially contains only the module header and a set of opcode
-// definitions for each of the 10 ALU operations that are to be implemented.
 //
 // The ALU operates on two operands using the operator defined by exe_alu_opc_r.
 //
@@ -29,15 +27,6 @@
 // should remove the 'reg' declaration keyword from the output port definition
 // of alu_result.
 //
-// Your ALU instructions can be tested incrementally, in four groups of
-// instructions, by setting the following macros in the tb.v testbench file
-// to either 1 (to enable) or 0 (to disable).
-//
-//  a) Logical operations  { AND, OR, XOR  } by setting ENABLE_LOGIC to 1 or 0
-//  b) Additive operations { ADD, SUB      } by setting ENABLE_ADD to 1 or 0
-//  c) Set operations      { SLT, SLTU     } by setting ENABLE_SET to 1 or 0
-//  d) Shift operations    { SLL, SRL, SRA } by setting ENABLE_SHIFTS to 1 or 0
-//
 ////////////////////////////////////////////////////////////////////////////////
 
 module alu(
@@ -49,18 +38,14 @@ module alu(
   output  reg [31:0]    alu_result      // ALU result output
 );
 
-localparam [3:0]  // logical
+localparam [3:0]  
                   ALU_OPC_AND  = 4'b0111, // bitwise AND
                   ALU_OPC_OR   = 4'b0110, // bitwise OR
                   ALU_OPC_XOR  = 4'b0100, // bitwise XOR
-                  // Additive
                   ALU_OPC_ADD  = 4'b0000, // 2's complement addition
                   ALU_OPC_SUB  = 4'b1000, // 2's complement subtraction
-                  // Set
                   ALU_OPC_SLT  = 4'b0010, // set if less than (signed)
                   ALU_OPC_SLTU = 4'b0011, // set if less than (unsigned)
-                  // Shift
-                  // The shift amount is given by the least-significant 5 bits of the second source operand - Nigel
                   ALU_OPC_SLL  = 4'b0001, // shift-left logical
                   ALU_OPC_SRL  = 4'b0101, // shift-right logical
                   ALU_OPC_SRA  = 4'b1101; // shift-right arithmetic
@@ -69,13 +54,35 @@ always @(*)
     begin
         // logical
         case(exe_alu_opc_r)
-        4'b: // 
-        alu_result = ; 
+        4'b0111: // bitwise AND
+        alu_result = exe_reg1_r & exe_src2_r; 
+        4'b0110: // bitwise OR
+        alu_result = exe_reg1_r | exe_src2_r; 
+        4'b0100: // bitwise XOR
+        alu_result = exe_reg1_r ^ exe_src2_r; 
+        
         // Additive: ignore overflow
-            
+        4'b0000: // 2's complement addition
+            begin
+                
+                alu_result = ; 
+            end
+        4'b1000: // 2's complement subtraction
+        alu_result = ; 
+        
         // Set
-            
+        4'b0010: // set if less than (signed)
+        alu_result = ; 
+        4'b0011: // set if less than (unsigned)
+        alu_result = ; 
+        
         // Shift: shift amount is given by the least-significant 5 bits of the second source operand - Nigel
+        4'b0001: // shift-left logical
+        alu_result = ; 
+        4'b0101: // shift-right logical
+        alu_result = ; 
+        4'b1101: // shift-right arithmetic
+        alu_result = ; 
         
         default: alu_result = 32'b0; 
     endcase
