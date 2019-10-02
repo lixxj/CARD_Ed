18664d015c21a432165f6de9fa2aa9ab363ee208
Xj Li <s2003300@petterson.inf.ed.ac.uk>
2019-10-02 16:44:54 +0100

    prac1 done, report todo

---
+++ prac1/proj/CARD_P1.runs/synth_1/.Xil/alu_propImpl.xdc
@@ -0,0 +1,5 @@
+set_property SRC_FILE_INFO {cfile:/afs/inf.ed.ac.uk/user/s20/s2003300/Desktop/Coursework/CARD/prac1/src/constraints/timing.xdc rfile:../../../../src/constraints/timing.xdc id:1} [current_design]
+set_property src_info {type:XDC file:1 line:4 export:INPUT save:INPUT read:READ} [current_design]
+set_input_jitter [all_clocks] 0.0
+set_property src_info {type:XDC file:1 line:5 export:INPUT save:INPUT read:READ} [current_design]
+set_clock_uncertainty 0.0 [all_clocks]
---
+++ prac1/proj/CARD_P1.runs/synth_1/runme.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# 
+# Vivado(TM)
+# runme.sh: a Vivado-generated Runs Script for UNIX
+# Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+# 
+
+if [ -z "$PATH" ]; then
+  PATH=/disk/scratch/Xilinx/SDK/2018.3/bin:/disk/scratch/Xilinx/Vivado/2018.3/ids_lite/ISE/bin/lin64:/disk/scratch/Xilinx/Vivado/2018.3/bin
+else
+  PATH=/disk/scratch/Xilinx/SDK/2018.3/bin:/disk/scratch/Xilinx/Vivado/2018.3/ids_lite/ISE/bin/lin64:/disk/scratch/Xilinx/Vivado/2018.3/bin:$PATH
+fi
+export PATH
+
+if [ -z "$LD_LIBRARY_PATH" ]; then
+  LD_LIBRARY_PATH=/disk/scratch/Xilinx/Vivado/2018.3/ids_lite/ISE/lib/lin64
+else
+  LD_LIBRARY_PATH=/disk/scratch/Xilinx/Vivado/2018.3/ids_lite/ISE/lib/lin64:$LD_LIBRARY_PATH
+fi
+export LD_LIBRARY_PATH
+
+HD_PWD='/afs/inf.ed.ac.uk/user/s20/s2003300/Desktop/Coursework/CARD/prac1/proj/CARD_P1.runs/synth_1'
+cd "$HD_PWD"
+
+HD_LOG=runme.log
+/bin/touch $HD_LOG
+
+ISEStep="./ISEWrap.sh"
+EAStep()
+{
+     $ISEStep $HD_LOG "$@" >> $HD_LOG 2>&1
+     if [ $? -ne 0 ]
+     then
+         exit
+     fi
+}
+
+EAStep vivado -log alu.vds -m64 -product Vivado -mode batch -messageDb vivado.pb -notrace -source alu.tcl
