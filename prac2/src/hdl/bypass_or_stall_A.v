eb1f895af036a4ad67dac7693015ff3fa9d1e031
lixxj <z5190719@ad.unsw.edu.au>
2019-10-21 16:55:30 +0100

    prac2.a done

--- prac2/src/hdl/bypass_or_stall_A.v
+++ prac2/src/hdl/bypass_or_stall_A.v
@@ -9,7 +9,6 @@
 // license from Nigel Topham.
 //
 // The entire notice above must be reproduced on all authorized copies.
-//
 ////////////////////////////////////////////////////////////////////////////////
 
 module bypass_or_stall(
@@ -61,27 +60,27 @@ module bypass_or_stall(
 always @*
     begin : bypass_stall_PROC
         
-        // reset status
+        ////////// reset status //////////
         dec_stall = 1'b0;
         dec_load_use = 1'b0;
         dec_csr_use = 1'b0;
         
-        // rs1 status
+        ////////// rs1 status //////////
         dec_stall = dec_stall || (dec_rs1 == exe_rd && exe_rd_wenb && dec_rs1_renb);
         dec_load_use = dec_load_use || (dec_rs1 == exe_rd && exe_rd_wenb && dec_rs1_renb && exe_load);
         dec_csr_use = dec_csr_use || (dec_rs1 == exe_rd && exe_rd_wenb && dec_rs1_renb && exe_csr);       
         dec_stall = dec_stall || (dec_rs1 == mem_rd && mem_rd_wenb && dec_rs1_renb);
         dec_stall = dec_stall || (dec_rs1 == wrb_rd && wrb_rd_wenb && dec_rs1_renb);
         
-        // rs2 status
+        ////////// rs2 status //////////
         dec_stall = dec_stall || (dec_rs2 == exe_rd && exe_rd_wenb && dec_rs2_renb);
         dec_load_use = dec_load_use || (dec_rs2 == exe_rd && exe_rd_wenb && dec_rs2_renb && exe_load);
         dec_csr_use = dec_csr_use || (dec_rs2 == exe_rd && exe_rd_wenb && dec_rs2_renb && exe_csr);      
         dec_stall = dec_stall || (dec_rs2 == mem_rd && mem_rd_wenb && dec_rs2_renb);
         dec_stall = dec_stall || (dec_rs2 == wrb_rd && wrb_rd_wenb && dec_rs2_renb);
         
-        dec_rs1_data    = dec_rdata1;
-        dec_rs2_data    = dec_rdata2;
+        dec_rs1_data = dec_rdata1;
+        dec_rs2_data = dec_rdata2;
     
     end // bypass_stall_PROC of PART A
 
